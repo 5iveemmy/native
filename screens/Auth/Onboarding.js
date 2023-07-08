e414ffb555ee8cbd7a8e6f180ff4e394c79d21f8
5iveemmy <ainemmanuel87@gmail.com>
2023-07-08 23:24:05 +0100

    chore: add background image to onboarding component

--- screens/Auth/Onboarding.js
+++ screens/Auth/Onboarding.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { ImageBackground } from "react-native";
 import { Text } from "react-native";
-import { View } from "react-native";
+import { padding } from "../../utils/helper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import onboarding from "../../assets/images/onboarding.png";
 
 const Onboarding = () => {
   return (
-    <View>
-      <Text>Welcome to aisles</Text>
-    </View>
+    <ImageBackground
+      source={onboarding}
+      resizeMode="cover"
+      style={{ background: "no-repeat" }}
+    >
+      <SafeAreaView style={{ ...padding(0, 40, 0, 40) }}>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+        <Text>Welcome to aisles</Text>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
