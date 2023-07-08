27f5a4e21c59ef8d148cb651c1e47084a6c1168c
5iveemmy <ainemmanuel87@gmail.com>
2023-07-08 09:49:00 +0100

    feat: install react native dependencies

---
+++ screens/Auth/Onboarding.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { View } from "react-native";
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Welcome to aisles</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
