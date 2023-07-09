30211f97c153522ce81b1d9531154ddf829b8dd3
5iveemmy <ainemmanuel87@gmail.com>
2023-07-09 15:20:14 +0100

    chore: implementt onboarding component

--- screens/Auth/Onboarding.js
+++ screens/Auth/Onboarding.js
@@ -4,27 +4,54 @@ import { Text } from "react-native";
 import { padding } from "../../utils/helper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import onboarding from "../../assets/images/onboarding.png";
+import { COLORS } from "../../constansts";
+import Button from "../../components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 const Onboarding = () => {
+  const navigation = useNavigation();
   return (
     <ImageBackground
       source={onboarding}
       resizeMode="cover"
-      style={{ background: "no-repeat" }}
+      style={{
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingBottom: 100,
+      }}
     >
-      <SafeAreaView style={{ ...padding(0, 40, 0, 40) }}>
-        <Text>Welcome uoooo aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
-        <Text>Welcome to aisles</Text>
+      <SafeAreaView
+        style={{
+          ...padding(0, 40, 0, 40),
+        }}
+      >
+        <Text style={{ color: COLORS["white-01"], fontSize: 39 }}>
+          Welcome to aisles
+        </Text>
+        <Text
+          style={{
+            color: COLORS["white-01"],
+            fontSize: 20,
+            ...padding(24, 0, 24, 0),
+          }}
+        >
+          Become a Picker agent, click the button below to continue.
+        </Text>
+        <Button
+          text="Continue"
+          handlePress={() => navigation.navigate("SignUp")}
+        />
+        <Text
+          style={{
+            color: COLORS.primary,
+            fontSize: 16,
+            paddingTop: 30,
+            textDecorationLine: "underline",
+            textAlign: "center",
+          }}
+        >
+          I have an account
+        </Text>
       </SafeAreaView>
     </ImageBackground>
   );
